23c37e3f97be86056b90bf25214977efa7e8b357
Luca vercelli <luca.vercelli.to@gmail.com>
2018-01-18 17:23:42 +0100

    Better description

--- dbmanager/__manifest__.py
+++ dbmanager/__manifest__.py
@@ -7,12 +7,15 @@
 
 {
     "name": "Database Manager",
-    "version": "1.0",
+    "version": "1.0.1",
     "category": "Configuration",
     "description": """
 		Allow execution of arbitrary SQL commands on DB.
 		
 		This module can be useful in order to execute **simple** queries/updates on database without direct access to server, and/or without PgAdmin installed.
+		
+		The module create a new menu "Configuration" -> "Technical" -> "DB Manager", that is visible only in development mode.
+		Only members of group "Database (SQL) manager" can use it.
 		""",
     "author": "Luca Vercelli - Finsoft srl",
     "website": "",
