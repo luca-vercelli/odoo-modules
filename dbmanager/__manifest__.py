f2e277158af5f32586075546f3228c150bb05996
Luca vercelli <luca.vercelli.to@gmail.com>
2018-01-18 14:12:06 +0100

    DB manager v.0

---
+++ dbmanager/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+# Released under LGPL v.3
+#
+##############################################################################
+
+{
+    "name": "Database Manager",
+    "version": "1.0",
+    "category": "Configuration",
+    "description": """
+		Allow execution of arbitrary SQL commands on DB.
+		
+		This module can be useful in order to execute **simple** queries/updates on database without direct access to server, and/or without PgAdmin installed.
+		""",
+    "author": "Luca Vercelli - Finsoft srl",
+    "website": "",
+    "depends": [
+        "base"
+    ],
+    'data': [
+        'views/views.xml',
+        'views/create_views.sql',
+    ],
+    "init_xml": [],
+    "update_xml": [],
+    "demo_xml": [],
+    "test": [],
+    "installable": True,
+    "active": False,
+	"license" : 'LGPL-3',
+	"application" : False,
+}
