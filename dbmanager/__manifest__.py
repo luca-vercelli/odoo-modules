53d03221aec341b0ac7a390e58a3283564b2db4e
Luca Vercelli <luca.vercelli.to@gmail.com>
2018-10-04 23:03:51 +0200

    descriptions

--- dbmanager/__manifest__.py
+++ dbmanager/__manifest__.py
@@ -9,14 +9,7 @@
     "name": "Database Manager",
     "version": "1.0.2",
     "category": "Configuration",
-    "description": """
-		Allow execution of arbitrary SQL commands on DB.
-		
-		This module can be useful in order to execute **simple** queries/updates on database without direct access to server, and/or without PgAdmin installed.
-		
-		The module create a new menu "Configuration" -> "Technical" -> "DB Manager", that is visible only in development mode.
-		Only members of group "Database (SQL) manager" can use it.
-		""",
+    "description": "",
     "author": "Luca Vercelli - Finsoft srl",
     "website": "",
     "depends": [
--- marca_da_bollo/__manifest__.py
+++ marca_da_bollo/__manifest__.py
@@ -9,18 +9,7 @@
     "name": "Marca da bollo.",
     "version": "1.1",
     "category": "Accounting",
-    "description": """
-      Addebita la marca da bollo in fattura, in maniera ingenua: sempre, se l'importo è sufficiente.
-
-      Dopo aver installato il modulo, occorre configurarlo:
-
-      * selezionare il flag "addebita marca da bollo" sui clienti opportuni
-
-      * impostare il conto della tassa marca da bollo, dal menù Imposte. Ad esempio il 260100 (IVA n/debito) dovrebbe funzionare, anche se non è molto corretto.
-      
-      * Il valore limite di default è 77,47€, si può modificare tramite il menu Imposte
-      
-      """,
+    "description": "",
     "author": "Luca Vercelli",
     "website": "",
     "depends": [
