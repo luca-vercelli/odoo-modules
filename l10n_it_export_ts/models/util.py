57eb3e8fbe6bac339c3cfa6fffe5958087f774b7
Luca Vercelli <luca.vercelli.to@gmail.com>
2018-10-14 16:19:05 +0200

    refactor

--- l10n_it_export_ts/models/util.py
+++ l10n_it_export_ts/models/util.py
@@ -19,45 +19,13 @@
 #
 ##############################################################################
 
-from .. import osa
-
-#See:
-#http://www.rgs.mef.gov.it/VERSIONE-I/Attivit--i/Spesa-soci/Progetto-T/
-#http://sistemats1.sanita.finanze.it/wps/wcm/connect/ea33a237-e840-4cdc-8a62-3a763831cf83/DM+31+luglio+2015.pdf?MOD=AJPERES&CACHEID=ea33a237-e840-4cdc-8a62-3a763831cf83
-#http://sistemats1.sanita.finanze.it/wps/wcm/connect/487b0bba-6a65-42f9-8b43-2fb907fe7e91/730+Spese+Sanitarie+-+WS+Asincrono+-+Invio+dati+di+spesa++sanitaria+%2819+settembre+2016%29.pdf?MOD=AJPERES&CACHEID=487b0bba-6a65-42f9-8b43-2fb907fe7e91
-#
-#kit:
-#http://sistemats1.sanita.finanze.it/wps/wcm/connect/0f7a758c-9638-4e74-95b7-bde0bfab29a5/kit730P_ver_20161117.zip?MOD=AJPERES&CACHEID=0f7a758c-9638-4e74-95b7-bde0bfab29a5
-
-# <<Il trattamento e la conservazione del codice fiscale dell'assistito,
-#rilevato dalla Tessera Sanitaria, crittografato secondo le
-#modalita' di cui al decreto attuativo del comma 5 dell'articolo 50
-#del DL 269/2003, utilizzando la chiave pubblica RSA contenuta
-#nel certificato X.509 fornito dal sistema TS ed applicando il
-#padding PKCS#1 v 1.5. Tale trattamento deve essere eseguito
-#tramite procedure automatizzate all'atto della memorizzazione
-#negli archivi locali.>>
-
-# i due indirizzi sono questi:
-# URL_TEST="https://invioss730ptest.sanita.finanze.it/InvioTelematicoSS730pMtomWeb/InvioTelematicoSS730pMtomPort"
-# URL_PROD="https://invioss730p.sanita.finanze.it/InvioTelematicoSS730pMtomWeb/InvioTelematicoSS730pMtomPort"
-# il problema e' che ?wsdl *non* viene reso disponibile via web
-# quindi abbiamo i due file WSDL in locale:
-
 import os
 DIRNAME = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-XSD_FILENAME = os.path.join(DIRNAME, "data", "730_precompilata.xsd")
-WSDL_PROD = os.path.join(DIRNAME, "data", "InvioTelematicoSpeseSanitarie730p.wsdl")
-WSDL_TEST = os.path.join(DIRNAME, "data", "InvioTelematicoSpeseSanitarie730pTest.wsdl")
-WSDL_ESITO = os.path.join(DIRNAME, "data", "EsitoInvioDatiSpesa730Service.wsdl")
-WSDL_DET_ERRORI = os.path.join(DIRNAME, "data", "DettaglioErrori730Service.wsdl")
-WSDL_RICEVUTE = os.path.join(DIRNAME, "data", "RicevutaPdf730Service.wsdl")
 
 PUBLIC_KEY_PEM_FILENAME = os.path.join(DIRNAME, "data", "SanitelCF.cer")
 PUBLIC_KEY = None
 
-def get_pubkey():
+def get_pubkey(PUBLIC_KEY_PEM_FILENAME):
 	"""
 	Extract public RSA key from X.509 PEM certificate specified by PUBLIC_KEY_PEM_FILENAME
 	"""
@@ -128,167 +96,8 @@ def zip_single_file(orig_filename):
 	zf.write(orig_filename)
 	zf.close()
 	return zip_filename
-	
-def call_ws_invio(zipfilename, pincode_inviante, cf_proprietario, pwd, if_test):
-	"""
-	Call the webservice "inviaFileMtom()".
-	Fill all required parameters: file name, file content, owner data, basic auth. credentials
-	Currently, require a modified version of osa (as we need Basic Authentication)
-	Currently, MTOM protocol is not used, file is sent as part of the message.
-	
-	@return webservice answer, which is an object of type "inviaFileMtomResponse"
-	"""
-	global WSDL_PROD, WSDL_TEST
-	if if_test:
-		wsdl = WSDL_TEST
-	else:
-		wsdl = WSDL_PROD
 
-	cl = osa.Client(wsdl)
-
-	parameters = cl.types.inviaFileMtom()
-	parameters.nomeFileAllegato = os.path.basename(zipfilename)
-	parameters.pincodeInvianteCifrato = encrypt(pincode_inviante)
-	parameters.datiProprietario = cl.types.proprietario()
-	parameters.datiProprietario.cfProprietario = cf_proprietario	#cleartext
-	parameters.documento = open(zipfilename, "r").read()
-
-	cl.service.inviaFileMtom.set_auth(cf_proprietario, pwd)
-
-	return cl.service.inviaFileMtom(parameters)
-
-#Questa era una delle risposte:
-#(ricevutaInvio){
-#    codiceEsito = 000
-#    dataAccoglienza = 25-12-2016 22:24:20
-#    descrizioneEsito = Il file  in attesa di elaborazione, per conoscerne l'esito  necessario verificare la ricevuta
-#    dimensioneFileAllegato = 24922
-#    nomeFileAllegato = file1.xmlI4c52M.zip
-#    protocollo = 16122522242096203
-#    idErrore = 
-#}
-
-def call_ws_esito(pincode_inviante, protocollo, cf_proprietario, pwd):
-	"""
-	Call the webservice "EsitoInvii()".
-	Restituisce l'esito dell'invio corrispondente al numero di protocollo dato
-	@return webservice answer
-	"""
-	global WSDL_ESITO
-
-	wsdl = WSDL_ESITO
-	cl = osa.Client(wsdl)
-
-	parameters = cl.types.EsitoInvii()
-	parameters.DatiInputRichiesta = cl.types.datiInput()
-	parameters.DatiInputRichiesta.pinCode = encrypt(pincode_inviante)
-	parameters.DatiInputRichiesta.protocollo = protocollo
-	#alternativi al protocollo:
-	#parameters.DatiInputRichiesta.dataInizio = '24-12-2016'
-	#parameters.DatiInputRichiesta.dataFine = '26-12-2016'
-
-	cl.service.EsitoInvii.set_auth(cf_proprietario, pwd)
-
-	return cl.service.EsitoInvii(parameters)
-
-#Questa era una delle risposte:
-#(datiOutput){
-#    esitoChiamata = 0
-#    descrizioneEsito =  
-#    esitiPositivi = (esitiPositivi){
-#                        dettagliEsito[] = [
-#                                           (dettaglioEsitoPositivo){
-#                                               protocollo = 16122522242096203
-#                                               dataInvio = 25-12-2016 22:24:20
-#                                               stato = 5
-#                                               descrizione = File scartato in fase di Elaborazione
-#                                               nInviati = 0
-#                                               nAccolti = 0
-#                                               nWarnings = 0
-#                                               nErrori = 0
-#                                           }
-#                                           ]
-#                    }
-#    esitiNegativi = None (esitiNegativi)
-#}
-
-def call_ws_dettaglio_errori(pincode_inviante, protocollo, cf_proprietario, pwd):
-	"""
-	Call the webservice "DettaglioErrori()".
-	Restituisce un CSV contenente il dettaglio degli errori di importazione
-	@return (webservice answer, csv_filename)
-	"""
-	global WSDL_DET_ERRORI
-
-	wsdl = WSDL_DET_ERRORI
-	cl = osa.Client(wsdl)
-
-	parameters = cl.types.DettaglioErrori()
-	parameters.DatiInputRichiesta = cl.types.datiInput()
-	parameters.DatiInputRichiesta.pinCode = encrypt(pincode_inviante)
-	parameters.DatiInputRichiesta.protocollo = protocollo
-
-	cl.service.DettaglioErrori.set_auth(cf_proprietario, pwd)
-
-	answer = cl.service.DettaglioErrori(parameters)
-
-	csv_filename = None
-	try:
-		if answer.esitiPositivi.dettagliEsito.csv:
-			import tempfile
-			boh, csv_filename = tempfile.mkstemp(prefix="errori", suffix=".csv.zip")
-			fd = open(csv_filename,"w")
-			fd.write(answer.esitiPositivi.dettagliEsito.csv)
-			fd.close()
-	except:
-		pass
-	return (answer, csv_filename)
-
-#Questa era una delle risposte:
-#(datiOutput){
-#    esitoChiamata = 1
-#    esitiPositivi = None (esitiPositivi)
-#    esitiNegativi = (esitiNegativi){
-#                        dettaglioEsitoNegativo[] = [
-#                                                    (dettaglioEsitoNegativo){
-#                                                        codice = WS37
-#                                                        descrizione = FILE SCARTATO, CONTROLLARE LA RICEVUTA PDF
-#                                                    }
-#                                                    ]
-#                    }
-#}
-
-def call_ws_ricevuta(pincode_inviante, protocollo, cf_proprietario, pwd):
-	"""
-	Call the webservice "RicevutaPdf()".
-	Restituisce la ricevuta dell'invio corrispondente al numero di protocollo dato
-	@return (webservice answer, local PDF temp file)
-	"""
-	global WSDL_RICEVUTE
-
-	wsdl = WSDL_RICEVUTE
-	cl = osa.Client(wsdl)
-
-	parameters = cl.types.RicevutaPdf()
-	parameters.DatiInputRichiesta = cl.types.datiInput()
-	parameters.DatiInputRichiesta.pinCode = encrypt(pincode_inviante)
-	parameters.DatiInputRichiesta.protocollo = protocollo
-
-	cl.service.RicevutaPdf.set_auth(cf_proprietario, pwd)
-
-	answer = cl.service.RicevutaPdf(parameters)
-
-	pdf_filename = None
-	if answer.esitiPositivi and answer.esitiPositivi.dettagliEsito and answer.esitiPositivi.dettagliEsito.pdf:
-		import tempfile
-		boh, pdf_filename = tempfile.mkstemp(prefix="ricevuta", suffix=".pdf")
-		fd = open(pdf_filename,"w")
-		fd.write(answer.esitiPositivi.dettagliEsito.pdf)
-		fd.close()
-	
-	return (answer, pdf_filename)
-
-def test_xsd(xml_filename, xsd_filename=XSD_FILENAME):
+def test_xsd(xml_filename, xsd_filename):
 	"""
 	Test XML against XSD schema.
 	Raise exception with details if validation fails. 
@@ -299,20 +108,4 @@ def test_xsd(xml_filename, xsd_filename=XSD_FILENAME):
 	doc = etree.parse(xml_filename)
 	#return xmlschema.validate(doc)
 	xmlschema.assertValid(doc)
-
-def upperLowerCase(xml_filename):
-	"""
-	I tag XML sono attesi case sensitive, invece Odoo li genera tutti minuscoli
-	Questa implementazione esegue SED, e richiede l'opzione s/././I, quindi funziona solo in Linux
-	"""
-	TAGS = ['cfProprietario','documentoSpesa','idSpesa','pIva','dataEmissione','numDocumentoFiscale','numDocumento','dataPagamento','flagOperazione','cfCittadino','voceSpesa','tipoSpesa']
-	SED = "sed -i "
-	for tag in TAGS:
-		SED += " -e s/%s/%s/I " % (tag, tag)
-	SED += xml_filename
-	try:
-		print("Executing ", SED)
-		os.system(SED)
-	except:
-		print("Errore nella conversione di maiuscole e minuscole, il file potrebbe venire rifiutato")
 
--- l10n_it_export_ts/models/wizards.py
+++ l10n_it_export_ts/models/wizards.py
@@ -65,6 +65,35 @@ class WizardExportInvoices(models.TransientModel):
             'messages': messages
             })
 
+#See:
+
+# <<Il trattamento e la conservazione del codice fiscale dell'assistito,
+#rilevato dalla Tessera Sanitaria, crittografato secondo le
+#modalita' di cui al decreto attuativo del comma 5 dell'articolo 50
+#del DL 269/2003, utilizzando la chiave pubblica RSA contenuta
+#nel certificato X.509 fornito dal sistema TS ed applicando il
+#padding PKCS#1 v 1.5. Tale trattamento deve essere eseguito
+#tramite procedure automatizzate all'atto della memorizzazione
+#negli archivi locali.>>
+
+# i due indirizzi sono questi:
+# URL_TEST="https://invioss730ptest.sanita.finanze.it/InvioTelematicoSS730pMtomWeb/InvioTelematicoSS730pMtomPort"
+# URL_PROD="https://invioss730p.sanita.finanze.it/InvioTelematicoSS730pMtomWeb/InvioTelematicoSS730pMtomPort"
+# il problema e' che ?wsdl *non* viene reso disponibile via web
+# quindi abbiamo i due file WSDL in locale:
+
+import os
+DIRNAME = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+XSD_FILENAME = os.path.join(DIRNAME, "data", "730_precompilata.xsd")
+WSDL_PROD = os.path.join(DIRNAME, "data", "InvioTelematicoSpeseSanitarie730p.wsdl")
+WSDL_TEST = os.path.join(DIRNAME, "data", "InvioTelematicoSpeseSanitarie730pTest.wsdl")
+WSDL_ESITO = os.path.join(DIRNAME, "data", "EsitoInvioDatiSpesa730Service.wsdl")
+WSDL_DET_ERRORI = os.path.join(DIRNAME, "data", "DettaglioErrori730Service.wsdl")
+WSDL_RICEVUTE = os.path.join(DIRNAME, "data", "RicevutaPdf730Service.wsdl")
+
+
+
 class WizardSendToTS(models.TransientModel):
     _name = "exportts.wizard.send"
     _description = "Invia XML a Sistema TS"
@@ -80,7 +109,7 @@ class WizardSendToTS(models.TransientModel):
         export = self.env['exportts.export.registry'].browse(self.env.context['active_id'])
         cf_proprietario = export.proprietario_id.fiscalcode # TODO puoi prendere quello già criptato
         from . import util
-        answer3, pdf_filename = util.call_ws_ricevuta(self.pincode_inviante, 16122814390642472, cf_proprietario, self.password_inviante)
+        answer3, pdf_filename = self.call_ws_ricevuta(self.pincode_inviante, 16122814390642472, cf_proprietario, self.password_inviante)
         self.pdf_filename = pdf_filename
         print("Ricevuta PDF salvata in:", pdf_filename)
         #os.system("xdg-open " + str(pdf_filename))
@@ -90,7 +119,7 @@ class WizardSendToTS(models.TransientModel):
         export = self.env['exportts.export.registry'].browse(self.env.context['active_id'])
         cf_proprietario = export.proprietario_id.fiscalcode # TODO puoi prendere quello già criptato
         from . import util
-        answer3, csv_filename = util.call_ws_dettaglio_errori(self.pincode_inviante, 16122814390642472, cf_proprietario, self.password_inviante)
+        answer3, csv_filename = self.call_ws_dettaglio_errori(self.pincode_inviante, 16122814390642472, cf_proprietario, self.password_inviante)
         self.csv_filename = csv_filename
         print("Ricevuta CSV salvata in:", csv_filename)
         #os.system("xdg-open " + str(csv_filename))
@@ -110,16 +139,13 @@ class WizardSendToTS(models.TransientModel):
         TEST = (self.endpoint == 'T')
 
         from . import util
-        print("Converting uppercase & lowercase...")
-        #TODO spostare altrove?
-        util.upperLowerCase(xmlfilename)
         print("Validating...")
-        util.test_xsd(xmlfilename)
+        util.test_xsd(xmlfilename, XSD_FILENAME)
         print("Compressione dati...")
         zipfilename = util.zip_single_file(xmlfilename)
         
         print("Invio dati...")
-        answer = util.call_ws_invio(zipfilename, self.pincode_inviante, cf_proprietario, self.password_inviante, TEST)
+        answer = self.call_ws_invio(zipfilename, self.pincode_inviante, cf_proprietario, self.password_inviante, TEST)
 
         print("Invio concluso. Risposta:")
         print(answer)
@@ -130,10 +156,10 @@ class WizardSendToTS(models.TransientModel):
             import time
             time.sleep(4)
             print("Esito invio:")
-            answer2 = util.call_ws_esito(self.pincode_inviante, protocollo, cf_proprietario, self.password_inviante)
+            answer2 = self.call_ws_esito(self.pincode_inviante, protocollo, cf_proprietario, self.password_inviante)
             print(answer2)
             
-            answer3, pdf_filename = util.call_ws_ricevuta(self.pincode_inviante, protocollo, cf_proprietario, self.password_inviante)
+            answer3, pdf_filename = self.call_ws_ricevuta(self.pincode_inviante, protocollo, cf_proprietario, self.password_inviante)
             print("Ricevuta PDF salvata in:", pdf_filename)
             self.pdf_filename = pdf_filename
             #import os
@@ -146,6 +172,175 @@ class WizardSendToTS(models.TransientModel):
             #import os
             #if csv_filename is not None:
             #    os.system("xdg-open " + str(csv_filename))
+
+    def call_ws_invio(self, zipfilename, pincode_inviante, cf_proprietario, pwd, if_test):
+	    """
+	    Call the webservice "inviaFileMtom()".
+	    Fill all required parameters: file name, file content, owner data, basic auth. credentials
+	    Currently, require a modified version of osa (as we need Basic Authentication)
+	    Currently, MTOM protocol is not used, file is sent as part of the message.
+	    
+	    @return webservice answer, which is an object of type "inviaFileMtomResponse"
+	    """
+
+        from .. import osa
+
+	    global WSDL_PROD, WSDL_TEST
+	    if if_test:
+		    wsdl = WSDL_TEST
+	    else:
+		    wsdl = WSDL_PROD
+
+	    cl = osa.Client(wsdl)
+
+	    parameters = cl.types.inviaFileMtom()
+	    parameters.nomeFileAllegato = os.path.basename(zipfilename)
+	    parameters.pincodeInvianteCifrato = encrypt(pincode_inviante)
+	    parameters.datiProprietario = cl.types.proprietario()
+	    parameters.datiProprietario.cfProprietario = cf_proprietario	#cleartext
+	    parameters.documento = open(zipfilename, "r").read()
+
+	    cl.service.inviaFileMtom.set_auth(cf_proprietario, pwd)
+
+	    return cl.service.inviaFileMtom(parameters)
+
+    #Questa era una delle risposte:
+    #(ricevutaInvio){
+    #    codiceEsito = 000
+    #    dataAccoglienza = 25-12-2016 22:24:20
+    #    descrizioneEsito = Il file  in attesa di elaborazione, per conoscerne l'esito  necessario verificare la ricevuta
+    #    dimensioneFileAllegato = 24922
+    #    nomeFileAllegato = file1.xmlI4c52M.zip
+    #    protocollo = 16122522242096203
+    #    idErrore = 
+    #}
+
+    def call_ws_esito(self, pincode_inviante, protocollo, cf_proprietario, pwd):
+	    """
+	    Call the webservice "EsitoInvii()".
+	    Restituisce l'esito dell'invio corrispondente al numero di protocollo dato
+	    @return webservice answer
+	    """
+	    global WSDL_ESITO
+
+        from .. import osa
+
+	    wsdl = WSDL_ESITO
+	    cl = osa.Client(wsdl)
+
+	    parameters = cl.types.EsitoInvii()
+	    parameters.DatiInputRichiesta = cl.types.datiInput()
+	    parameters.DatiInputRichiesta.pinCode = encrypt(pincode_inviante)
+	    parameters.DatiInputRichiesta.protocollo = protocollo
+	    #alternativi al protocollo:
+	    #parameters.DatiInputRichiesta.dataInizio = '24-12-2016'
+	    #parameters.DatiInputRichiesta.dataFine = '26-12-2016'
+
+	    cl.service.EsitoInvii.set_auth(cf_proprietario, pwd)
+
+	    return cl.service.EsitoInvii(parameters)
+
+    #Questa era una delle risposte:
+    #(datiOutput){
+    #    esitoChiamata = 0
+    #    descrizioneEsito =  
+    #    esitiPositivi = (esitiPositivi){
+    #                        dettagliEsito[] = [
+    #                                           (dettaglioEsitoPositivo){
+    #                                               protocollo = 16122522242096203
+    #                                               dataInvio = 25-12-2016 22:24:20
+    #                                               stato = 5
+    #                                               descrizione = File scartato in fase di Elaborazione
+    #                                               nInviati = 0
+    #                                               nAccolti = 0
+    #                                               nWarnings = 0
+    #                                               nErrori = 0
+    #                                           }
+    #                                           ]
+    #                    }
+    #    esitiNegativi = None (esitiNegativi)
+    #}
+
+    def call_ws_dettaglio_errori(self, pincode_inviante, protocollo, cf_proprietario, pwd):
+	    """
+	    Call the webservice "DettaglioErrori()".
+	    Restituisce un CSV contenente il dettaglio degli errori di importazione
+	    @return (webservice answer, csv_filename)
+	    """
+	    global WSDL_DET_ERRORI
+
+        from .. import osa
+
+	    wsdl = WSDL_DET_ERRORI
+	    cl = osa.Client(wsdl)
+
+	    parameters = cl.types.DettaglioErrori()
+	    parameters.DatiInputRichiesta = cl.types.datiInput()
+	    parameters.DatiInputRichiesta.pinCode = encrypt(pincode_inviante)
+	    parameters.DatiInputRichiesta.protocollo = protocollo
+
+	    cl.service.DettaglioErrori.set_auth(cf_proprietario, pwd)
+
+	    answer = cl.service.DettaglioErrori(parameters)
+
+	    csv_filename = None
+	    try:
+		    if answer.esitiPositivi.dettagliEsito.csv:
+			    import tempfile
+			    boh, csv_filename = tempfile.mkstemp(prefix="errori", suffix=".csv.zip")
+			    fd = open(csv_filename,"w")
+			    fd.write(answer.esitiPositivi.dettagliEsito.csv)
+			    fd.close()
+	    except:
+		    pass
+	    return (answer, csv_filename)
+
+    #Questa era una delle risposte:
+    #(datiOutput){
+    #    esitoChiamata = 1
+    #    esitiPositivi = None (esitiPositivi)
+    #    esitiNegativi = (esitiNegativi){
+    #                        dettaglioEsitoNegativo[] = [
+    #                                                    (dettaglioEsitoNegativo){
+    #                                                        codice = WS37
+    #                                                        descrizione = FILE SCARTATO, CONTROLLARE LA RICEVUTA PDF
+    #                                                    }
+    #                                                    ]
+    #                    }
+    #}
+
+    def call_ws_ricevuta(self, pincode_inviante, protocollo, cf_proprietario, pwd):
+	    """
+	    Call the webservice "RicevutaPdf()".
+	    Restituisce la ricevuta dell'invio corrispondente al numero di protocollo dato
+	    @return (webservice answer, local PDF temp file)
+	    """
+	    global WSDL_RICEVUTE
+
+        from .. import osa
+
+	    wsdl = WSDL_RICEVUTE
+	    cl = osa.Client(wsdl)
+
+	    parameters = cl.types.RicevutaPdf()
+	    parameters.DatiInputRichiesta = cl.types.datiInput()
+	    parameters.DatiInputRichiesta.pinCode = encrypt(pincode_inviante)
+	    parameters.DatiInputRichiesta.protocollo = protocollo
+
+	    cl.service.RicevutaPdf.set_auth(cf_proprietario, pwd)
+
+	    answer = cl.service.RicevutaPdf(parameters)
+
+	    pdf_filename = None
+	    if answer.esitiPositivi and answer.esitiPositivi.dettagliEsito and answer.esitiPositivi.dettagliEsito.pdf:
+		    import tempfile
+		    boh, pdf_filename = tempfile.mkstemp(prefix="ricevuta", suffix=".pdf")
+		    fd = open(pdf_filename,"w")
+		    fd.write(answer.esitiPositivi.dettagliEsito.pdf)
+		    fd.close()
+	    
+	    return (answer, pdf_filename)
+
 
 class WizardEncryptAllFiscalCodes(models.TransientModel):
     _name = "res.partner.encrypt"
