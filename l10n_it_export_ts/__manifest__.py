9f713a6fe5d011a35e333aa7a792960f91632acb
Luca Vercelli <luca.vercelli@consulenti.csi.it>
2025-01-30 08:27:34 +0100

    groups

--- l10n_it_export_ts/__manifest__.py
+++ l10n_it_export_ts/__manifest__.py
@@ -12,7 +12,7 @@
 
 {
     'name': 'Send Invoices to Sistema TS',
-    'version': '16.0.3.0.0',
+    'version': '16.0.4.0.0',
     'category': 'Localization/Italy',
     'author': 'Luca Vercelli',
     'website': 'https://github.com/luca-vercelli/odoo-modules',
